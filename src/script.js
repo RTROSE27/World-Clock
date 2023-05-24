7f078c36b0e381b1e216561ba28b56e5fe00429e
RTROSE27 <128533621+RTROSE27@users.noreply.github.com>
2023-05-24 10:09:55 +0100

    Update script.js

---
+++ src/script.js
@@ -0,0 +1,5 @@
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+losAngelesDateElement.innerHTML = "May 24th 2023";
+losAngelesTimeElement.innerHTML = "1:48:15 <small>AM</small>";
