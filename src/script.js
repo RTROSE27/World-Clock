684e2c4cfd0d0311ee6b6471821ded71324ec69f
RTROSE27 <128533621+RTROSE27@users.noreply.github.com>
2023-05-25 14:18:06 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,12 +1,14 @@
-let losAngelesElement = document.querySelector("#los-angeles");
-let losAngelesDateElement = losAngelesElement.querySelector(".date");
-let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-let losAngelesTime = moment().tz("America/Los_Angeles");
+setInterval(function () {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
-losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
-  "h:mm:ss"
-)} <small>${losAngelesTime.format("A")}</small>`;
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
+    "h:mm:ss"
+  )} <small>${losAngelesTime.format("A")}</small>`;
+}, 1000);
 
 let parisElement = document.querySelector("#paris");
 let parisDateElement = parisElement.querySelector(".date");
